13f8dfa9b3a65a48d0d23f075842632f0bd11f80
Jay <1095143545@qq.com>
2019-08-03 10:56:03 +0800

    添加管理员列表

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -5,15 +5,21 @@ namespace app\admin\controller;
 
 
 use think\App;
+use app\common\model\Admin as AdminModel;
 
 class Admin extends Base
 {
+    public static $filed = 'id,user_name,nick_name,join_ip,avatar,create_time,update_time,status';
     public function __construct(App $app = null) {
         parent::__construct($app);
     }
 
 
     public function index() {
+        $adminList = AdminModel::field(self::$filed)->paginate();
+        $pages = $adminList->render();
+        $this->assign('page',$pages);
+        $this->assign('admin_list', $adminList);
 
         return $this->fetch();
     }
@@ -23,5 +29,9 @@ class Admin extends Base
         return $this->fetch();
     }
 
+    public function edit($id){
+
+        return $this->fetch();
+    }
 
 }
