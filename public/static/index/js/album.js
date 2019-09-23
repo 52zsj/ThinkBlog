82a5f19b00376dd8b05651fc4763cf6667b60298
LucklyJack <1095143545@qq.com>
2019-09-23 09:46:44 +0800

    调整相册，调整模板

--- public/static/index/js/album.js
+++ public/static/index/js/album.js
@@ -8,7 +8,8 @@ define(['jquery', 'scrollreveal', 'layui', 'template'], function ($, scrollrevea
         },
         detail: function () {
             //引入相册展示插件JS
-            require(['albumplugin'], function (albumplugin) {});
+            require(['albumplugin'], function (albumplugin) {
+            });
         },
         api: {
             bindevent: function () {
@@ -19,32 +20,37 @@ define(['jquery', 'scrollreveal', 'layui', 'template'], function ($, scrollrevea
         },
         events: {
             lay_page: function (total = 0) {
-                layui.use('laypage', function () {
-                    var laypage = layui.laypage;
-                    //执行一个laypage实例
-                    laypage.render({
-                        elem: 'pagelist' //注意，这里的 test1 是 ID，不用加 # 号
-                        , count: total //数据总数，从服务端得到
-                        , limit: 8
-                        , theme: '#1E9FFF'
-                        , curr: location.hash.replace('#!page=', '') //获取hash值为page的当前页
-                        , hash: 'page' //自定义hash值
-                        , jump: function (obj) {
-                            var offset = obj.curr;//当前页
-                            lucklyJack.api.ajax({
-                                url: 'album.html',
-                                type: "POST",
-                                data: {offset: offset},
-                                loading: false,
-                            }, false, function (res) {
-                                var htmls = template('album-item', res);
-                                console.log(htmls);
-                                $("#picbox").html(htmls);
-                            });
-                        }
+                if (total > 0) {
+                    layui.use('laypage', function () {
+                        var laypage = layui.laypage;
+                        //执行一个laypage实例
+                        laypage.render({
+                            elem: 'pagelist' //注意，这里的 test1 是 ID，不用加 # 号
+                            , count: total //数据总数，从服务端得到
+                            , limit: 8
+                            , theme: '#1E9FFF'
+                            , curr: location.hash.replace('#!page=', '') //获取hash值为page的当前页
+                            , hash: 'page' //自定义hash值
+                            , jump: function (obj) {
+                                var offset = obj.curr;//当前页
+                                lucklyJack.api.ajax({
+                                    url: 'album.html',
+                                    type: "POST",
+                                    data: {offset: offset},
+                                    loading: false,
+                                }, false, function (res) {
+                                    var htmls = template('album-item', res);
+                                    console.log(htmls);
+                                    $("#picbox").html(htmls);
+                                });
+                            }
 
+                        });
                     });
-                });
+                } else {
+                    var htmls = '<div style=" text-align: center;color: red;line-height: 350px;font-size: 25px;">相册内容为空...</div>';
+                    $("#picbox").html(htmls);
+                }
             },
             animation: function (tag) {
                 window.sr = scrollreveal({reset: true});
