ece43c47cf503c9dc50cfb25da0c2403c7e51880
Jack <1095143545@qq.com>
2019-10-10 21:54:44 +0800

    添加标签文章列表

--- application/index/controller/Article.php
+++ application/index/controller/Article.php
@@ -3,6 +3,8 @@
 
 namespace app\index\controller;
 
+use app\common\exception\Failure;
+use app\common\exception\Success;
 use app\common\model\Article as ArticleModel;
 use app\common\model\ArticleTags as ArticleTagsModel;
 use app\common\model\Inspirational;
@@ -11,6 +13,8 @@ use app\index\logic\Widget as WidgetLogic;
 
 class Article extends Base
 {
+    protected $pageSize = 8;
+
     public function initialize() {
         parent::initialize();
     }
@@ -51,8 +55,8 @@ class Article extends Base
         WidgetLogic::inspirational();
         WidgetLogic::hotArticle();
         //相关推荐
-        $likeArticle = ArticleModel::where('column_id','eq',$articleInfo['column_id'])->field('id,title,cover')->limit(6)->select();
-        $this->assign('like_article',$likeArticle);
+        $likeArticle = ArticleModel::where('column_id', 'eq', $articleInfo['column_id'])->field('id,title,cover')->limit(6)->select();
+        $this->assign('like_article', $likeArticle);
         $data['title'] = $articleInfo['title'];
         $data['webKeywords'] = $articleInfo['title'];
         $data['webDescription'] = $articleInfo['description'];
@@ -69,24 +73,34 @@ class Article extends Base
 
     public function tagList() {
         $tagId = $this->request->param('tag_id');
-        if (!$tagId) {
-            $this->error('异常操作');
+        if (!empty($tagId) && !is_numeric($tagId)) {
+            $this->error('参数异常');
         }
-        $taginfo = TagModel::where('status', 'eq', 1)->value('name');
-        if (empty($taginfo)) {
-            $this->error('该标签已被停用');
-        }
-        $articleList = ArticleTagsModel::where('tag_id', 'eq', $tagId)->with(['articleList' => function ($query) {
-            $query->field('id,title,cover,description')->where('status', 'eq', 1)->order('order_key asc , create_time desc')->limit(10);
-        }])->select();
+        WidgetLogic::inspirational();
+        WidgetLogic::hotArticle();
+        if ($this->request->isAjax()) {
+            $offset = $this->request->param('offset');
+            $taginfo = TagModel::where('status', 'eq', 1)->value('name');
+            if (empty($taginfo)) {
+                throw new Failure('该标签已被停用了~', '', url('index/index/index'));
+            }
+            $where[] = ['tag_id ', 'eq', $tagId];
+            //总数
+            $articleTotal = ArticleTagsModel::where($where)->with(['articleList' => function ($query) {
+                $query->where('status', 'eq', 1);
+            }])->count();
 
-        if (!empty($articleList)) {
-            $articleList = array_chunk($articleList->toArray(), 2);
+            $articleList = ArticleTagsModel::where($where)->with(['articleList' => function ($query) {
+                $query->field('id,title,cover,description')->where('status', 'eq', 1)->order('order_key asc , create_time desc');
+            }])->page($offset, $this->pageSize)->select();
+            //处理url
+            foreach ($articleList as $k => &$v) {
+                $v['detail_url'] = url('index/article/detail', ['article_id' => $v['article_id']]);
+            }
+            throw new Success('请求成功', ['row' => $articleList, 'total' => $articleTotal, 'limit' => $this->pageSize,'info'=>$taginfo]);
+
         }
-        $assignArray = [
-            'article_list' => $articleList,
-        ];
-        $this->assign($assignArray);
+        $this->assignConfig('request_url', url('index/article/tagList', ['tag_id' => $tagId]));
         return $this->fetch();
 
     }
--- public/static/index/js/article.js
+++ public/static/index/js/article.js
@@ -2,13 +2,12 @@ define(['jquery', 'layui', 'template'], function ($, layui, template) {
     var Controller = {
         detail: function () {
             Controller.api.bindevent();
-            $(".related-content ul li").hover(function () {
-                $(this).find("h3").show();
-            }, function () {
-                $(this).find("h3").hide();
-            });
+            Controller.events.hover();
         },
+        taglist: function () {
+            Controller.events.lay_page();
 
+        },
         api: {
             bindevent: function () {
                 common.events.no_image();
@@ -19,6 +18,74 @@ define(['jquery', 'layui', 'template'], function ($, layui, template) {
             },
         },
         events: {
+            lay_page: function (offset = '') {
+                layui.use('laypage', function () {
+                    var laypage = layui.laypage;
+                    //严格点
+                    offset = offset == '' ? 1 : offset;
+                    lucklyJack.api.ajax({
+                        url: Controller.api.get_request_url(),
+                        type: "POST",
+                        data: {offset: offset},
+                        loading: false,
+                    }, false, function (data) {
+                        var str =' {{each row item i}}\n' +
+                            '        {{if item.article_list}}\n' +
+                            '        <li>\n' +
+                            '            <a href="{{item.detail_url}}">\n' +
+                            '                <img src="\\{{item.article_list.cover}}" alt="">\n' +
+                            '                <h3>{{item.article_list.title}}</h3>\n' +
+                            '            </a>\n' +
+                            '        </li>\n' +
+                            '        {{/if}}\n' +
+                            '    {{/each}}';
+                        var htmls = template.render(str,data);
+                        var limit = data.limit;
+                        var total = data.total;
+                        if (total > limit) {
+                            laypage.render({
+                                elem: 'pagelist', //注意，这里的 test1 是 ID，不用加 # 号
+                                count: total,//数据总数，从服务端得到
+                                limit: limit,
+                                theme: '#1E9FFF',
+                                curr: location.hash.replace('#!page=', ''),//获取起始页
+                                hash: 'page', //自定义hash值
+                                jump: function (obj, first) {
+                                    var offset = obj.curr;//当前页
+                                    if (!first) {
+                                        Controller.events.lay_page(offset);
+                                    }
+                                }
+                            });
+                        } else {
+                            $("#pagelist").empty();
+                        }
+                        $("#article-item-list").html(htmls);
+                        $(".artile-item .title strong").removeClass('hide');
+                        $(".artile-item .title strong b").eq(0).text(data.info);
+                        $(".artile-item .title strong b").eq(1).text(data.total);
+                        //重新渲染
+                        common.events.random_color();
+                        common.events.no_image();
+                        Controller.events.hover();
+
+                    },function (data,ret) {
+                        layer.msg(ret.msg, {icon: 5}, function () {
+                            location.href = ret.jump_url;
+                        });
+
+                    });
+                    return false;
+                });
+
+            },
+            hover:function () {
+                $(".related-content ul li").hover(function () {
+                    $(this).find("h3").show();
+                }, function () {
+                    $(this).find("h3").hide();
+                });
+            }
 
         }
 
