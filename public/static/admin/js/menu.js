7c559c039e3bc85276830f17448ef916fa5fbf98
LucklyJack <1095143545@qq.com>
2019-10-21 10:15:35 +0800

    添加图标搜索

--- public/static/admin/js/menu.js
+++ public/static/admin/js/menu.js
@@ -1,4 +1,4 @@
-define(['xadmin', 'form', 'jack.table'], function (xadmin, Form, Table) {
+define(['xadmin', 'form', 'jack.table', 'template'], function (xadmin, Form, Table, template) {
     var Controller = {
             index: function () {
                 var option = {
@@ -13,13 +13,13 @@ define(['xadmin', 'form', 'jack.table'], function (xadmin, Form, Table) {
                     url: 'menu/index',//数据接口
                     title: '后台菜单',
                     toolbar: '#tool-bar',
-                    page:false,
+                    page: false,
                     cols: [[ //表头
                         {type: 'checkbox', width: '5%'},
                         {field: 'id', title: 'ID'},
                         {field: 'title', title: '内容'},
                         {field: 'url', title: '路径'},
-                        {field: 'icon', title: '图标',templet: '#icon'},
+                        {field: 'icon', title: '图标', templet: '#icon'},
                         {field: 'create_time', title: '创建时间'},
                         {field: 'update_time', title: '创建时间'},
                         {field: 'right', title: '操作', toolbar: '#operate', fixed: 'right'},
@@ -31,11 +31,14 @@ define(['xadmin', 'form', 'jack.table'], function (xadmin, Form, Table) {
                 Controller.api.bindevent();
             },
             add: function () {
+
                 Controller.api.bindevent();
+                Controller.events.icon_search();
 
             },
             edit: function () {
                 Controller.api.bindevent();
+                Controller.events.icon_search();
 
             },
             api: {
@@ -44,7 +47,43 @@ define(['xadmin', 'form', 'jack.table'], function (xadmin, Form, Table) {
                 },
             },
             events: {
+                icon_search: function () {
+                    var icon_list = [];
+                    var open_index;
+                    var iconfunc = function () {
+                        open_index = Layer.open({
+                            type: 1,
+                            area: ['99%', '98%'], //宽高
+                            content: template('choose_icon', {icon_list: icon_list})
+                        });
+                    };
+                    $(document).on('click', ".btn-search-icon", function () {
+                        if (icon_list.length == 0) {
+                            $.get("/static/libs/font-awesome/less/variables.less", function (ret) {
+                                var exp = /fa-var-(.*):/ig;
+                                var result;
+                                while ((result = exp.exec(ret)) != null) {
+                                    icon_list.push(result[1]);
+                                }
+                                console.log(icon_list);
+                                iconfunc();
 
+                            });
+                        } else {
+                            iconfunc();
+                        }
+                    });
+                    $(document).on('click', '#chooseicon ul li', function () {
+                        $("input[name='icon']").val('fa fa-' + $(this).data("font"));
+                        Layer.close(open_index);
+                    });
+                    $(document).on('keyup', 'input.js-icon-search', function () {
+                        $("#chooseicon ul li").show();
+                        if ($(this).val() != '') {
+                            $("#chooseicon ul li:not([data-font*='" + $(this).val() + "'])").hide();
+                        }
+                    });
+                }
             }
 
         }
